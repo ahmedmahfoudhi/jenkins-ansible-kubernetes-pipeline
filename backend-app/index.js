7bbb87b7d54e0dc32ed5e9d5ca4f02a46adba96a
ahemdmahfoudhi <mahfoudhiahmed2000@gmail.com>
2023-11-19 16:26:39 +0100

    initial commit

---
+++ backend-app/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+const app = express()
+dotenv.config()
+
+const connect = async (retries = 10) => {
+    if(!retries){
+        console.log(`Could not connect to the Database`);
+        return;
+    }
+    console.log(`Trying to connect to the DB - Try number ${11 - retries}`)
+    try {
+        await mongoose.connect(process.env.MONGODB_SERVER)
+        console.log(`Successfully connected to the Database - ${process.env.DB_URI}`)
+    } catch (error) {
+        console.log(error);
+        setTimeout(async () => await connect(retries-1), 5000);
+    }
+}
+
+const port = process.env.PORT || 3000
+
+app.get('/', (_,res) => {
+    res.status(200).send("Welcome!")
+})
+
+app.listen(port, async () => {
+    console.log(`Server is listening on port ${port}`)
+    await connect();
+})
